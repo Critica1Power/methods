9135aaeba97b5c94821edf08243d98518532260e
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-19 00:09:21 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,30 @@ public class Main {
             System.out.printf("%s - не високосный год\n", year);
         }
     }
+
+    public void checkAndPrintSuitableOs(int clientOS, int year) {
+        if (clientOS == 0 && year == 2022) {
+            System.out.println("Вам нужна современная версия для IOS");
+        } else if (clientOS == 0 && year < 2022) {
+            System.out.println("Вам нужна Lite версия для IOS");
+        } else if (clientOS == 1 && year == 2022) {
+            System.out.println("Вам нужна современная версия для Android");
+        } else if (clientOS == 0 && year < 2022) {
+            System.out.println("Вам нужна Lite версия для Android");
+        } else {
+            System.out.println("Ваша ОС не поддерживается");
+        }
+    }
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
         int year = 2022;
         Main main = new Main();
         main.isLeapYearAndPrint(year);
+        // Задание 2
+        System.out.println("Задание 2");
+        int clientOs = 0;
+        main.checkAndPrintSuitableOs(clientOs, year);
 
     }
 }
