5d07c9d862a05d0a8a4fdb8c061a5317c0d53ef1
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-18 23:53:42 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
+    public void isLeapYearAndPrint(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.printf("%s - високосный год\n", year);
+        } else {
+            System.out.printf("%s - не високосный год\n", year);
+        }
+    }
     public static void main(String[] args) {
         // Задание 1
         System.out.println("Задание 1");
+        int year = 2022;
+        Main main = new Main();
+        main.isLeapYearAndPrint(year);
+
     }
 }
